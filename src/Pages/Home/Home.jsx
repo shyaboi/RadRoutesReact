1e0b7c3637445253dd0bbc1bfa64fbd06f4686f2
Shyaboi <ian.shane.sears@gmail.com>
2021-03-15 02:22:32 -0700

    posting files with type selctor

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -21,16 +21,26 @@ const Home = (props) => {
         <Col>
           This is the home page
           <NavLink href="/">logo</NavLink>
-          <Form method='POST' action="http://localhost:5000/file" >
-          <FormGroup>
-            <Label for="exampleFile">File</Label>
-            <Input type="file" name="file" id="exampleFile" />
-            <FormText color="muted">
-              This is some placeholder block-level help text for the above
-              input. It's a bit lighter and easily wraps to a new line.
-            </FormText>
-          </FormGroup>
-          <Button>Submit</Button>
+          <Form method="POST" action="http://localhost:5000/file" enctype="multipart/form-data">
+            <FormGroup>
+              <Label for="exampleFile">File</Label>
+              <Input type="file" name="file" id="exampleFile" />
+              <FormText color="muted">
+                This is some placeholder block-level help text for the above
+                input. It's a bit lighter and easily wraps to a new line.
+              </FormText>
+            </FormGroup>
+            <FormGroup>
+        <Label for="exampleSelect">Select</Label>
+        <Input type="select" name="type" id="exampleSelect">
+          <option name='python' value="python">Python</option>
+          <option name='ruby' value="ruby">Ruby</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup>
+            <Button>Submit</Button>
           </Form>
         </Col>
       </Row>
