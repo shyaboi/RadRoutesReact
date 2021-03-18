19dda56f2300409c12ee7f73d05015043cf01006
Shyaboi <ian.shane.sears@gmail.com>
2021-03-17 23:42:01 -0700

    made more pages, have upload file, editor, see uploaded code , and home and splash page

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+  FormGroup,
+  Input,
+} from "reactstrap";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import rad from "../../assets/images/rad.png";
+import routes from "../../assets/images/routes.png";
+
+const Navi = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {}, []);
+
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div>
+      <Navbar
+        light
+        expand="md"
+        className="fixed-top muhNav radGrad font-weight-bold "
+      >
+        <a href="/">
+          <NavbarBrand className="ml-4 brand" href="/">
+            <LazyLoadImage
+              id="pics"
+              effect="blur"
+              src={rad} // use normal <img> attributes as props
+              height="70px"
+              width="100px"
+            />
+          </NavbarBrand>
+        </a>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto " navbar>
+            <NavItem className='border border-primary'>
+              <NavLink href="/Home">
+                <strong>Home</strong>
+              </NavLink>
+            </NavItem>
+            <NavItem className="border border-primaryGrad">
+              <NavLink href="/Editor">
+              <strong>Editor</strong>
+                </NavLink>
+            </NavItem>
+
+            <NavItem className="border border-secondary">
+              <NavLink href="/FileUpload">
+              <strong>Upload a File</strong>
+                </NavLink>
+            </NavItem>
+            
+            <NavItem className="border border-secondaryGrad">
+              <NavLink href="/Peep">
+              <strong>Peep Code</strong>
+                </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>
+            {/* <Form> */}
+
+            <a href="/">
+              <NavbarBrand className="ml-4 brand" href="/">
+                <LazyLoadImage
+                  id="pics"
+                  effect="blur"
+                  src={routes} // use normal <img> attributes as props
+                  height="55px"
+                  width="130px"
+                />
+              </NavbarBrand>
+            </a>
+            {/* </Form> */}
+          </NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navi;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,119 +1,27 @@
-import React, { useState, useEffect } from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  NavLink,
-} from "reactstrap";
-import { AvailableAddOnContext } from "twilio/lib/rest/preview/marketplace/availableAddOn";
-import logo from "../../assets/images/rr.png";
-import fetchy from "../../Utils/Fetcher";
+import React, { useEffect } from "react";
+import { NavLink } from "reactstrap";
+import {Container, Row, Col} from "reactstrap";
+import Navi from '../../Components/Nav/Nav'
 
-const Home = (props) => {
-  const [type, setType] = useState("...Waiting for file to be loaded");
-  const [route, setRoute] = useState("my-rad-route");
-  const [routeExists, setExitance] = useState('Availible');
-  const [avail, setAvail] = useState('avail');
-
-  const extChange = (e) => {
-    let fileInput = e.target.files[0].name;
-    let popFile = fileInput.split(".").pop();
-    console.log(popFile);
-    setType(popFile);
-  };
-
-  const ok = (rr) => {
-    fetchy(`http://localhost:5000/exists/${rr}`).then(async (data) => {
-      console.log(data);
-      let d = data;
-      if(d===false){
-        setExitance('Route Availible')
-        setAvail('avail')
-      }
-      if(d===true){
-        setExitance('Route NOT Availible')
-        setAvail('notAvail')
-      }
-    });
-  };
-
-  const routeChange = (e) => {
-    let r = e.target.value;
-    setRoute(r);
-    ok(r);
-  };
-
-  useEffect(() => {}, []);
+function Home() {
+  useEffect(() => {
+  }, []);
 
   return (
-    <Container className="pb-5 txt-cen ninja">
-      <Form
-        method="POST"
-        action="http://localhost:5000/file"
-        encType="multipart/form-data"
-      >
-        <Row className="txt-cen">
-          <Col>
-            <NavLink href="/">
-              <img src={logo} alt="rad routes logo" />
-            </NavLink>
-          </Col>
-        </Row>
-        <Row className="txt-cen" xl="2" xs="1">
-          <Col className="ninja p-5">
-            <FormGroup>
-              <Label>
-                Your Rad Route will be hosted at https://radroutes.com/{route}
-              </Label>
-              <Input
-                type="text"
-                name="route"
-                placeholder="my-rad-route"
-                onChange={(e) => {
-                  routeChange(e);
-                }}
-              />
-            </FormGroup>
-            <Label className={avail}>
-            Your Route is {routeExists}
-            </Label>
-          </Col>
+      <Container>
+          <Container>
 
-          <Col className="ninja p-5">
-            <FormGroup>
-              <Label for="exampleFile">Upload a file for your route:</Label>
-              <Input
-                type="file"
-                onChange={(e) => {
-                  extChange(e);
-                }}
-                name="file"
-                id="exampleFile"
-              />
-              <FormText color="muted">Your File is a {type} File</FormText>
-            </FormGroup>
-            <FormGroup className="ninjaVanish">
-              <Input type="select" name="type" id="exampleSelect">
-                <option name="type" value={type}></option>
-              </Input>
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button size="lg" className="ninja mt-5">
-              Submit
-            </Button>
-          </Col>
-        </Row>
-      </Form>
+    <Navi/>
+          </Container>
+    <Container className="mt-5 pt-5 txt-cen">
+    <Row>
+        <Col>
+        things
+        </Col>
+    </Row>
     </Container>
+</Container>
   );
-};
+}
+
 export default Home;
