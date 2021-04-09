121fd527f669acc6ee1d04b03b45aaed7d6eeebe
Shyaboi <ian.shane.sears@gmail.com>
2021-04-08 22:13:37 -0700

    reset urls to rr

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -29,7 +29,7 @@ const Login = (props) => {
         'email': email,
         'password': pass,
       })
-      axios.post('http://localhost:5000/login', bod)
+      axios.post('https://radroutes.com/login', bod)
       .then(function (response) {
         let rData = response.data
         let authed = response.data.authed
