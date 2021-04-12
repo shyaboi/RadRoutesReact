6ecd9d1211dced76721f88774cca264f8c4a3dae
Shyaboi <ian.shane.sears@gmail.com>
2021-04-12 02:40:31 -0700

    hosting docs on netlify subdomain, changed link to docs

--- src/Pages/Splash/Splash.jsx
+++ src/Pages/Splash/Splash.jsx
@@ -49,7 +49,7 @@ const Splash = (props) => {
                 </Button>
               </NavLink>
             </Fade>
-              <NavLink href='https://tender-goodall-ad5f05.netlify.app/'>
+              <NavLink href='https://docs.radroutes.com/'>
                 <Button
                   className='m-2 p-3'
                   size='lg'
